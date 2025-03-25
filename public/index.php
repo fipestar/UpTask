4a6ec99df31b50364d7d4661edf19f50cfef0fc3
juan david barbosa neira <you@example.com>
2025-03-25 10:33:45 -0500

    Añadir Tareas Proyecto

--- public/index.php
+++ public/index.php
@@ -5,6 +5,8 @@ require_once __DIR__ . '/../includes/app.php';
 use MVC\Router;
 use Controllers\LoginController;
 use Controllers\DashboardController;
+use Controllers\TareaController;
+
 $router = new Router();
 
 //Login
@@ -34,5 +36,11 @@ $router->get('/crear-proyecto', [new DashboardController(), 'crear_proyecto']);
 $router->post('/crear-proyecto', [new DashboardController(), 'crear_proyecto']);
 $router->get('/perfil', [new DashboardController(), 'perfil']);
 $router->get('/proyecto', [new DashboardController(), 'proyecto']);
+
+//Api para las tareas
+$router->get('/api/tareas', [new TareaController(), 'index']);
+$router->post('/api/tarea', [new TareaController(), 'crear']);
+$router->post('/api/tarea/actualizar', [new TareaController(), 'actualizar']);
+$router->post('/api/tarea/eliminar', [new TareaController(), 'eliminar']);
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
--- src/js/tareas.js
+++ src/js/tareas.js
@@ -19,7 +19,7 @@
                     />
             </div>
             <div class="opciones">
-                <input type="submit" class="submit-nueva-tarea" value="Añadir Tarea"/>
+                <input type="submit" class="submit-nueva-tarea" value="Añadir Tarea" />
                 <button type="button" class="cerrar-modal">Cancelar</button>
             </div>
         </form>
@@ -27,7 +27,87 @@
         setTimeout(() => {
             const formulario = document.querySelector('.formulario');
             formulario.classList.add('animar');
-        }, 3000);
-        document.querySelector('body').appendChild(modal);
+        }, 0);
+
+        
+
+        modal.addEventListener('click', function(e){
+            e.preventDefault();
+            
+            if(e.target.classList.contains('cerrar-modal') || e.target.classList.contains('modal')){
+                const formulario = document.querySelector('.formulario');
+                formulario.classList.add('cerrar');
+                setTimeout(() => {
+                    modal.remove();
+                }, 500);
+            } 
+            if(e.target.classList.contains('submit-nueva-tarea')) {
+                submitFormularioNuevaTarea();   
+            }
+        })   
+        document.querySelector('.dashboard').appendChild(modal);
+    }
+
+    function submitFormularioNuevaTarea(){
+        const tarea = document.querySelector('#tarea').value.trim();
+
+        if(tarea === ''){
+            //Mostrar una alerta de error
+            mostrarAlerta('eL nombre de la tarea no puede estar vacio', 'error', document.querySelector('.formulario legend'));
+            return;
+        } 
+
+        agregarTarea(tarea);
+    }
+
+    //Muestra un mensaje en la interfaz
+    function mostrarAlerta(mensaje, tipo, referencia){
+        //Previene la creacion de multiples alertas
+        const alertaPrevia = document.querySelector('.alerta');
+        if(alertaPrevia){
+            alertaPrevia.remove();
+        }
+
+        const alerta = document.createElement('DIV');
+        alerta.classList.add('alerta', tipo);
+        alerta.textContent = mensaje;
+       
+        //Inserta la alerta antes del legend
+        referencia.parentElement.insertBefore(alerta, referencia.nextElementSibling);
+
+        //Eliminar la alerta despues de 5 segundos
+        setTimeout(() => {
+            alerta.remove();
+        }, 5000);
+    }
+    //Consultar el servidor para añadir una nueva tarea al proyecto actual
+    async function agregarTarea(tarea){
+        //Construir la peticion
+        const datos = new FormData();
+        datos.append('nombre', tarea);
+        datos.append('proyectoId', obtenerProyecto());
+
+        try {
+            const url = 'http://localhost:3000/api/tarea';
+            const respuesta = await fetch(url, {
+                method: 'POST',
+                body: datos
+            });
+
+            const resultado = await respuesta.json();
+            console.log(resultado);
+
+            mostrarAlerta(resultado.mensaje, resultado.tipo, document.querySelector('.formulario legend'));
+            return;
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    function obtenerProyecto(){
+        const proyectoParams = new URLSearchParams(window.location.search);
+        const proyecto = Object.fromEntries(proyectoParams.entries());
+        return proyecto.id;
     }
 })();

0a192a8eaa22dfa4ba4d9d740fd5e6b3ac88f2d0
juan david barbosa neira <you@example.com>
2025-04-01 10:18:36 -0500

    Cambiar Estado Tareas

--- controllers/TareaController.php
+++ controllers/TareaController.php
@@ -3,10 +3,21 @@
 namespace Controllers;
 
 use Model\Proyecto;
+use Model\Tareas;
 
 class TareaController {
     public static function index(){
+        $proyectoId = $_GET['id'];
 
+        if(!$proyectoId) header('Location: /dashboard');
+
+        $proyecto = Proyecto::where('url', $proyectoId);
+        session_start();
+        if(!$proyecto || $proyecto->propietarioId !== $_SESSION['id']) header('Location: /404');
+
+        $tareas = Tareas::belongsTo('proyectoId', $proyecto->id);
+
+        echo json_encode(['tareas' => $tareas]);
     }
 
     public static function crear(){
@@ -24,22 +35,52 @@ class TareaController {
                     'mensaje' => 'Hubo un Error al agregar la tarea'
                 ];
                 echo json_encode($respuesta);
+                return;
                 
-            } else {
-                $respuesta = [
-                    'tipo' => 'exito',
-                    'mensaje' => 'Tarea agregada exitosamente'
-                ];
-                echo json_encode($respuesta);
-            }
-            
-            
+            } 
+            //Todo bien, instanciar y crear la tarea
+            $tarea = new Tareas($_POST);
+            $tarea->proyectoId = $proyecto->id;
+            $resultado = $tarea->guardar();
+            $respuesta = [
+                'tipo' => 'exito',
+                'id' => $resultado['id'],
+                'mensaje' => 'Tarea Creada Correctamente',
+                'proyectoId' => $proyecto->id
+            ];
+            echo json_encode($respuesta);
         }
     }
 
     public static function actualizar(){
         if($_SERVER['REQUEST_METHOD'] === 'POST'){
-            
+            //Validar que el proyecto exista
+            $proyecto = Proyecto::where('url', $_POST['proyectoId']);
+
+            session_start();
+
+            if(!$proyecto || $proyecto->propietarioId !== $_SESSION['id']) {
+                $respuesta = [
+                    'tipo' => 'error',
+                    'mensaje' => 'Hubo un error al actualizar la tarea'
+                ];
+                echo json_encode($respuesta);
+                return;
+            }
+            $tarea = new Tareas($_POST);
+            $tarea->proyectoId = $proyecto->id;
+
+            $resultado = $tarea->guardar();
+            if($resultado) {
+                $respuesta = [
+                    'tipo' => 'exito',
+                    'id' => $tarea->id,
+                    'proyectoId' => $proyecto->id,
+                    'mensaje' => 'Actualizado Correctamente'
+                ];
+            }
+
+            echo json_encode(['respuesta' => $respuesta]);
         }
     }
 
--- src/js/tareas.js
+++ src/js/tareas.js
@@ -1,7 +1,79 @@
 (function(){
+    obtenerTareas();
+    let tareas = [];
     //Boton para mostrar el modal de agregar tarea
     const nuevaTareaBtn = document.querySelector('#agregar-tarea');
     nuevaTareaBtn.addEventListener('click', mostrarFormulario);
+
+    async function obtenerTareas(){
+        try {
+            const id = obtenerProyecto();
+            const url = `/api/tareas?id=${id}`;
+            const respuesta = await fetch(url);
+            const resultado = await respuesta.json();
+
+            tareas = resultado.tareas;
+            mostrarTareas();
+
+            
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    function mostrarTareas(){
+        limpiarTareas();
+        if(tareas.length === 0){
+            const contenedorTareas = document.querySelector('#listado-tareas');
+
+            const textoNoTareas = document.createElement('LI');
+            textoNoTareas.textContent = 'No hay Tareas';
+            textoNoTareas.classList.add('no-tareas');
+
+            contenedorTareas.appendChild(textoNoTareas);
+            return
+        }
+
+        const estados = {
+            0: 'Pendiente',
+            1: 'Completa'
+        }
+        tareas.forEach(tarea => {
+            const contenedorTarea = document.createElement('LI');
+            contenedorTarea.dataset.tareaId = tarea.id;
+            contenedorTarea.classList.add('tarea');
+
+            const nombreTarea = document.createElement('P');
+            nombreTarea.textContent = tarea.nombre;
+
+            const opcionesDiv = document.createElement('DIV');
+            opcionesDiv.classList.add('opciones');
+
+            //Botones
+            const btnEstadoTarea = document.createElement('BUTTON');
+            btnEstadoTarea.classList.add('estado-tarea');
+            btnEstadoTarea.classList.add(`${estados[tarea.estado].toLowerCase()}`)
+            btnEstadoTarea.textContent = estados[tarea.estado];
+            btnEstadoTarea.dataset.estadoTarea = tarea.estado;
+                btnEstadoTarea.ondblclick = function() {
+                    cambiarEstadoTarea({...tarea});
+                }
+
+            const btnEliminarTarea = document.createElement('BUTTON');
+            btnEliminarTarea.classList.add('eliminar-tarea');
+            btnEliminarTarea.dataset.idTarea = tarea.id;
+            btnEliminarTarea.textContent = 'Eliminar'
+
+            opcionesDiv.appendChild(btnEstadoTarea);
+            opcionesDiv.appendChild(btnEliminarTarea);
+
+            contenedorTarea.appendChild(nombreTarea);
+            contenedorTarea.appendChild(opcionesDiv);
+           
+            const listadoTareas = document.querySelector('#listado-tareas');
+            listadoTareas.appendChild(contenedorTarea);
+        })
+    }
 
     function mostrarFormulario() {
         const modal = document.createElement('DIV');
@@ -98,10 +170,68 @@
             console.log(resultado);
 
             mostrarAlerta(resultado.mensaje, resultado.tipo, document.querySelector('.formulario legend'));
-            return;
+            if(resultado.tipo === 'exito') {
+                const modal = document.querySelector('.modal');
+                setTimeout(() => {
+                    modal.remove();
+                                    
+                }, 3000);
+
+                //Agregar el objeto de tarea al global de tareas
+                const tareaObj = {
+                    id: String(resultado.id),
+                    nombre: tarea,
+                    estado: "0",
+                    proyectoId: resultado.proyectoId
+                }
+                tareas = [...tareas, tareaObj];
+                mostrarTareas();
+
+            }
 
         } catch (error) {
             console.log(error);
+        }
+    }
+
+    function cambiarEstadoTarea(tarea){
+        const nuevoEstado = tarea.estado === "1" ? "0" : "1";
+        tarea.estado = nuevoEstado
+        actualizarTarea(tarea);
+    }
+
+    async function actualizarTarea(tarea){
+        const {estado, id, nombre, proyectoId} = tarea;
+
+        const datos = new FormData();
+        datos.append('id', id);
+        datos.append('nombre', nombre);
+        datos.append('estado', estado);
+        datos.append('proyectoId', obtenerProyecto());
+
+        try {
+            const url = 'http://localhost:3000/api/tarea/actualizar';
+
+            const respuesta = await fetch(url, {
+                method: 'POST',
+                body: datos
+            });
+            const resultado = await respuesta.json();
+
+            if(resultado.respuesta.tipo === 'exito') {
+                mostrarAlerta(resultado.respuesta.mensaje, resultado.respuesta.tipo, document.querySelector('.contenedor-nueva-tarea'));
+            }
+
+            tareas = tareas.map(tareaMemoria => {
+                if(tareaMemoria.id === id) {
+                    tareaMemoria.estado = estado;
+                }
+                return tareaMemoria;
+            });
+
+            mostrarTareas();
+        } catch (error) {
+                console.log(error);
         }
     }
 
@@ -109,5 +239,12 @@
         const proyectoParams = new URLSearchParams(window.location.search);
         const proyecto = Object.fromEntries(proyectoParams.entries());
         return proyecto.id;
+    }
+
+    function limpiarTareas(){
+        const listadoTareas = document.querySelector('#listado-tareas');
+        while(listadoTareas.firstChild){
+            listadoTareas.removeChild(listadoTareas.firstChild);
+        }
     }
 })();
